9fc2a16aca4306868b9a22d31afc6ce4bb074b6a
Nikita souslov <n.souslau@student.maastrichtuniversity.nl>
2023-10-03 17:04:34 +0300

    I am BETTER

--- JavaCraft.java
+++ JavaCraft.java
@@ -27,7 +27,7 @@ public class JavaCraft {
   private static final String ANSI_BLUE = "\u001B[34m";
   private static final String ANSI_GRAY = "\u001B[37m";
   private static final String ANSI_WHITE = "\u001B[97m";
-
+//I like comments
   private static final String BLOCK_NUMBERS_INFO = "Block Numbers:\n" +
       "0 - Empty block\n" +
       "1 - Wood block\n" +
