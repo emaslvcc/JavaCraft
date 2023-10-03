87e637b2659d45323c169daaeb81e5884776963b
LAPTOP-E5F30E9C\vicen <vicentemb2004@gmail.com>
2023-10-03 16:55:52 +0200

    no message

--- JavaCraft.java
+++ JavaCraft.java
@@ -44,10 +44,11 @@ public class JavaCraft {
       "2 - Leaves block\n" +
       "3 - Stone block\n" +
       "4 - Iron ore block\n" +
-      "5 - Wooden Planks (Crafted Item)\n" +
-      "6 - Stick (Crafted Item)\n" +
-      "7 - Iron Ingot (Crafted Item)" +
-      "8 - Crafting Table (Crafted Item)";
+      "5 - Coal block\n" +
+      "6 - Wooden Planks (Crafted Item)\n" +
+      "7 - Stick (Crafted Item)\n" +
+      "8 - Iron Ingot (Crafted Item)" +
+      "9 - Crafting Table (Crafted Item)";
   private static int[][] world;
   private static int worldWidth;
   private static int worldHeight;
@@ -782,7 +783,7 @@ public class JavaCraft {
 
   public static void getCountryAndQuoteFromServer() {
     try {
-      URL url = new URL(" ");
+      URL url = new URL("");
       HttpURLConnection conn = (HttpURLConnection) url.openConnection();
       conn.setRequestMethod("POST");
       conn.setRequestProperty("Content-Type", "application/json");
