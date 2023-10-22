9d4a9e704d8e912fdce87cafe9e883306af6dbf7
Gallagher, Sheena (Stud. DACS) <sk.gallagher@student.maastrichtuniversity.nl>
2023-10-22 10:34:19 +0000

    FLAG

--- JavaCraft.java
+++ JavaCraft.java
@@ -189,11 +189,11 @@ public class JavaCraft {
       case STONE:
         return '\u2593';
       case WHITE_SQUARE:
-        return '\u25AE';
+        return '\u2588';
       case RED_SQUARE:
-        return '\u25AE';
+        return '\u2588';
       case BLUE_SQUARE:
-        return '\u25AE';
+        return '\u2588';
       case STAR:
         return '\u2605';
       case IRON_ORE:
